26069a05c0f0bcc5041929014a9668329bb64042
hedrickantonio <rhantoni.cuba@gmail.com>
2025-02-13 12:48:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+let arreglo = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let geros = ['ate', 'peed', 'crushed', 'broke'];
+let Amigo = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+let indicealeatorio = Math.floor(Math.random() * arreglo.length);
+let indicealeatorio1 = Math.floor(Math.random() * geros.length);
+let indicealeatorio2 = Math.floor(Math.random() * Amigo.length);
+let indicealeatorio3 = Math.floor(Math.random() * when.length);
+
+let valor =arreglo[indicealeatorio] +  " " + geros[indicealeatorio1] +  " " + Amigo[indicealeatorio2] +  " " + when[indicealeatorio3];
+
+console.log(valor);
+
+
+
   //write your code here
+  document.querySelector("#excuse").innerHTML=valor
   console.log("Hello Rigo from the console!");
 };
